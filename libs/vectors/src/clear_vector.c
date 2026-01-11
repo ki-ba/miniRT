ebd9e7b3ed60c65985c2e6554feef260232d1e54
aB-temps <187414947+aB-temps@users.noreply.github.com>
2026-01-11 16:56:05 +0100

    ➕ add(lib): resizeable array (vectors)

---
+++ libs/vectors/src/clear_vector.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clear_vector.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abetemps <abetemps@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/29 18:22:10 by abetemps          #+#    #+#             */
+/*   Updated: 2025/07/16 18:51:00 by abetemps         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vectors.h"
+#include <stdlib.h>
+
+void	clear_vector(t_vector **vec)
+{
+	if (!vec)
+		return;
+	if (*vec && (*vec)->array)
+	{
+		if ((*vec)->clear_array)
+			(*vec)->clear_array((*vec));
+		free((*vec)->array);
+		(*vec)->array = (void *)0;
+	}
+	if (*vec)
+	{
+		free(*vec);
+		*vec = (void *)0;
+	}
+}
