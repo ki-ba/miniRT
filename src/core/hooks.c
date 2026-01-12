8457727bad04744187e7fb3fffd0ebb8bcb37c1e
aB-temps <187414947+aB-temps@users.noreply.github.com>
2026-01-12 16:09:04 +0100

    ✨ feat(hooks): cam direction - mouse move

--- src/core/hooks.c
+++ src/core/hooks.c
@@ -11,8 +11,74 @@
 /* ************************************************************************** */
 
 #include "miniRT.h"
+#include "vec3.h"
 #include "render.h"
 #include "core.h"
+
+int	mouse_move(int x, int y, t_mini_rt *mini_rt)
+{
+	const double	cx = WIDTH / 2;
+	const double	cy = HEIGHT / 2;
+	static t_vec3	center = {cx, cy, 0};
+	const double	sensivity = .02;
+	const t_vec3	mouse = {x, y, VP_DISTANCE};
+	const t_vec3	dir = vec3_scale(vec3_normalize(vec3_substract(mouse, center)), sensivity);
+
+	// printf("x: %d | y: %d\n", x, y);
+	// printf("center: x: %f | y: %f | z: %f\n", center.x, center.y, center.z);
+	// printf("dir: x: %f | y: %f | z: %f\n", dir.x, dir.y, dir.z);
+
+	mini_rt->cam.dir = vec3_add(mini_rt->cam.dir, dir);
+	center = mouse;
+	// printf("cam.dir: x: %f | y: %f | z: %f\n", mini_rt->cam.dir.x, mini_rt->cam.dir.y, mini_rt->cam.dir.z);
+
+	shoot_rays(mini_rt);
+	return (0);
+}
+
+int	handle_mouse(int mouse_event, int x, int y, t_mini_rt	*mini_rt)
+{
+	double	step = 0.5;
+	(void) x;
+	(void) y;
+	
+
+	if (mouse_event == 4)
+		mini_rt->cam.ori = vec3_add(mini_rt->cam.ori, vec3_scale(mini_rt->cam.dir, step));
+	else if (mouse_event == 5)
+		mini_rt->cam.ori = vec3_substract(mini_rt->cam.ori, vec3_scale(mini_rt->cam.dir, step));
+	shoot_rays(mini_rt);
+	return (0);
+}
+
+int	handle_keypress(int keysym, t_mini_rt *mini_rt)
+{
+	double step = 0.5;
+
+	if (keysym == 65307 || keysym == 113)
+		clean_exit(mini_rt, SUCCESS);
+	else if (keysym == 119)
+		mini_rt->cam.ori = vec3_substract(mini_rt->cam.ori, vec3_scale(mini_rt->cam.up, step));
+	else if (keysym == 115)
+		mini_rt->cam.ori = vec3_add(mini_rt->cam.ori, vec3_scale(mini_rt->cam.up, step));
+	else if (keysym == 100)
+		mini_rt->cam.ori = vec3_add(mini_rt->cam.ori, vec3_scale(mini_rt->cam.right, step));
+	else if (keysym == 97)
+		mini_rt->cam.ori = vec3_substract(mini_rt->cam.ori, vec3_scale(mini_rt->cam.right, step));
+	else if (keysym == 114)
+	{
+		mini_rt->cam.ori.x = 0;
+		mini_rt->cam.ori.y = 0;
+		mini_rt->cam.ori.z = 0;
+		mini_rt->cam.dir.x = 0;
+		mini_rt->cam.dir.y = 0;
+		mini_rt->cam.dir.z = VP_DISTANCE;
+	}
+	// printf("Key pressed: %d\n", keysym);
+	// printf("Camera position: x=%f, y=%f, z=%f\n", mini_rt->cam.ori.x, mini_rt->cam.ori.y, mini_rt->cam.ori.z);
+	shoot_rays(mini_rt);
+	return (0);
+}
 
 /**
 	* @brief Handles keypress events.
@@ -21,39 +87,6 @@
 * @return 0 on success.
 * @note Exits the program if the Escape 65307 key / q 113 key is pressed.
 */
-int	handle_keypress(int keysym, t_mini_rt *mini_rt)
-{
-	double step = 0.5;
-	if (keysym == 65307 || keysym == 113)
-		clean_exit(mini_rt, SUCCESS);
-	else if (keysym == 119)
-		mini_rt->cam.ori.y += step;
-	else if (keysym == 115)
-		mini_rt->cam.ori.y -= step;
-	else if (keysym == 100)
-		mini_rt->cam.ori.x -= step;
-	else if (keysym == 97)
-		mini_rt->cam.ori.x += step;
-	else if (keysym == 65362)
-		mini_rt->cam.ori.z += step;
-	else if (keysym == 65364)
-		mini_rt->cam.ori.z -= step;
-	else if (keysym == 65361)
-		mini_rt->cam.dir.x += step;
-	else if (keysym == 65363)
-		mini_rt->cam.dir.x -= step;
-	else if (keysym == 114)
-	{
-		mini_rt->cam.ori.x = 0;
-		mini_rt->cam.ori.y = 0;
-		mini_rt->cam.ori.z = 0;
-	}
-	// printf("Key pressed: %d\n", keysym);
-	// printf("Camera position: x=%f, y=%f, z=%f\n", mini_rt->cam.ori.x, mini_rt->cam.ori.y, mini_rt->cam.ori.z);
-	shoot_rays(mini_rt);
-	return (0);
-}
-
 int	handle_window_close(t_mini_rt *mini_rt)
 {
 	clean_exit(mini_rt, SUCCESS);
